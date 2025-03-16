b5ba8480a7a0ea945465a82121e8ef95b0420a4e
Sean <seanshen12@gmail.com>
2025-03-15 19:55:13 -0700

    Finish Tailwind setup

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
   const t = useTranslations('Navigation');
 
   return (
-    <div className="bg-slate-850">
+    <div className="bg-red-500">
       <nav className="container flex justify-between p-2 text-white">
         <div>
           <NavigationLink href="/">{t('home')}</NavigationLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/**/*.{tsx,css}'],
-    theme: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        mono: [
-          'Monaco',
-          'ui-monospace',
-          'SFMono-Regular',
-          'Menlo',
-          'Consolas',
-          'Liberation Mono',
-          'Courier New',
-          'monospace'
-        ]
-      },
-      container: {
-        center: true,
-        screens: {
-          sm: '50rem'
-        }
-      },
-      extend: {
-        colors: {
-          slate: {
-            850: 'hsl(222deg 47% 16%)'
-          },
-          primary: '#5fc3e7'
-        }
-      }
-    },
-    plugins: []
-  };
-  
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Make sure this pattern matches your component files
+    './src/components/**/*.{js,jsx,ts,tsx}' // Explicitly include components directory
+  ],
+};
